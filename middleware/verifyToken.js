b02aa9016bcd0d054159e634a37fe8277f8587f4
Raul Candelario <isacel97@gmail.com>
2023-10-27 19:50:55 -0600

    middleware

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,13 +2,12 @@ const jwt = require('jsonwebtoken');
 const environment = require('../environment');
 const secret = environment.secret
 
-const verifyToken = (req, res, next) => {
+const verifyToken = async(req, res, next) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if(!token) return res.status(401).json({error: 'Access Denied'})
     try{
-        const verified = jwt.verify(token, secret);
-        res.status(400).json({data: verified});
+        await jwt.verify(token, secret);
     }catch(err){
         console.error(err);
         res.status(400).send('Invalid Token');
